91f51a67e7305e13dec00b37cf11f28bc8907084
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-02-12 10:05:09 +0200

    react installation, components & application, Dynamic values

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  //dyamic value
+  const link="https://www.google.com"; 
+  const title = 'Welcome to my blog page';
+  //we cant output objecta and boolean in react
+  // const person = {name: "Delyce", age: 19 };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="content">
+       <h1>{title}</h1>
+       {/* <p>{person}</p> */}
+       <p>{"hey"}</p>
+       {/* bunch all elements in a string together as an array */}
+       <p>{[1, 2, 3, 5]}</p>
+      {/* displaying a random number */}
+       <p>{Math.random() *10}</p>
+       <a href={link}> Google site</a>
+
+
+
+       
+     </div>
     </div>
   );
 }
