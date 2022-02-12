61b806b97116d7e4ce0e19b10ca6dcdefe5d6841
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-02-12 21:26:01 +0200

    fetching, creating json

--- src/home.js
+++ src/home.js
@@ -1,17 +1,28 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './bloglist' 
 const Home = () => {
-    const[blogs, setBlogs] = useState([
-        { title: 'Learn css with me', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ', author: 'Delyce', id:1 },
-        { title: 'Learn css with me', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ', author: 'Umuhire', id:2 },
-        { title: 'Learn css with me', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ', author: 'Delyce', id:3 },
-    ])
+    const[blogs, setBlogs] = useState(null);
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !==id);
+        setBlogs(newBlogs);
+
+    }
     
+    useEffect(() =>{
+        fetch('http://localhost:3005/blogs')
+        .then(res =>{
+
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data)
+        });
+    },[]) 
     
     return (
         <div className="home">
-            <BlogList blogs={blogs} title= "All blogs"/>
-            <BlogList blogs={blogs.filter((blog)=>blog.author=== "Delyce")} title= "Delyce's blogs"/>
+            {blogs &&  <BlogList blogs={blogs} title= "All blogs" />}
+           
 
         </div>
     )
