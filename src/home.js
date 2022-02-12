707357cc4a9dd7beef85bef1e7f37f518b256400
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-02-12 10:42:41 +0200

    event onclick

--- src/home.js
+++ src/home.js
@@ -1,9 +1,21 @@
 const Home = () => {
+    const handleClick = () =>{
+        console.log("Hello there!");
+    }
+    const handleClickAgain= (name) =>{
+        console.log("hello" + name);
+
+    }
     return (  
         <div className="home">
             <h2>Home page</h2>
+            {/* using handleClick() will invoke the function bcz of (), refresh ---- console the message */}
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() =>{
+                handleClickAgain(' Delyce')
+            } }>Click ma again</button>
         </div>
-    );
+    ); 
 }
  
 export default Home;
