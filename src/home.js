a66cdf700a2ccbaa1dc1affcaad6a3f3a75f7d60
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-02-12 15:35:58 +0200

    writing blogs in codes  and displaying them

--- src/home.js
+++ src/home.js
@@ -1,22 +1,22 @@
 import {useState} from 'react';
 const Home = () => {
+    const[blogs, setBlogs] = useState([
+        { title: 'Learn css with me', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ', author: 'Delyce', id:1 },
+        { title: 'Learn css with me', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ', author: 'Umuhire', id:2 },
+        { title: 'Learn css with me', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ', author: 'Delyce', id:3 },
+    ])
     
-    const [name, setName] = useState("Delyce")
-    const [age, setAge]= useState(19)
-    const handleClick = () =>{
-        setName('Umuhire');
-        setAge(20)
-      
-    }
-  
+    
     return (  
         <div className="home">
-            <h2>Home page</h2>
-            <p>{name} is {age} years old</p>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    </div>
+
+            ))}
             
-            {/* using handleClick() will invoke the function bcz of (), refresh ---- console the message */}
-            <button onClick={handleClick}>Click me</button>
-          
         </div>
     ); 
 }
