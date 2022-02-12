c9c1dd1bd02f0354d51344511b2a132a8fbd712c
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-02-12 17:36:56 +0200

    using props, filtering

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import BlogList from './bloglist' 
 const Home = () => {
     const[blogs, setBlogs] = useState([
         { title: 'Learn css with me', body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ', author: 'Delyce', id:1 },
@@ -7,18 +8,13 @@ const Home = () => {
     ])
     
     
-    return (  
+    return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    </div>
+            <BlogList blogs={blogs} title= "All blogs"/>
+            <BlogList blogs={blogs.filter((blog)=>blog.author=== "Delyce")} title= "Delyce's blogs"/>
 
-            ))}
-            
         </div>
-    ); 
+    )
 }
  
 export default Home;
